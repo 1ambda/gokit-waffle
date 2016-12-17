bb5627e2cf2744ad50e264c15cf61c6ba76357c6
1ambda <1amb4a@gmail.com>
2016-12-17 22:27:40 +0900

    chore: Parse env variables

--- waffle-server/main.go
+++ waffle-server/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"time"
+
+	arg "github.com/alexflint/go-arg"
 )
 
+// Build Flags
 var (
 	Version   = "undefined"
 	BuildTime = "undefined"
@@ -12,9 +15,19 @@ var (
 	Started   = time.Now()
 )
 
+// Env Variables
+var EnvVar struct {
+	Mode string `arg:"env"`
+}
+
 func main() {
 	fmt.Println(GitHash)
 	fmt.Println(BuildTime)
 	fmt.Println(Version)
 	fmt.Println(Started)
+
+	// Setup default values
+	EnvVar.Mode = "LOCAL" // [LOCAL, DEV, PROD]
+	arg.MustParse(&EnvVar)
+	fmt.Println(EnvVar.Mode)
 }
