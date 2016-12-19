b6ca2777a28c06df748c8f80652a78f4aca887f1
1ambda <1amb4a@gmail.com>
2016-12-20 00:56:47 +0900

    refactor: Add config package

--- waffle-server/main.go
+++ waffle-server/main.go
@@ -3,36 +3,21 @@ package main
 import (
 	"net/http"
 	"os"
-	"time"
 
+	"github.com/1ambda/gokit-waffle/waffle-server/config"
 	"github.com/1ambda/gokit-waffle/waffle-server/endpoint"
 	"github.com/1ambda/gokit-waffle/waffle-server/service"
 	"github.com/1ambda/gokit-waffle/waffle-server/transport"
 
-	arg "github.com/alexflint/go-arg"
 	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"golang.org/x/net/context"
 )
 
-// Build Flags
-var (
-	Version   = "undefined"
-	BuildTime = "undefined"
-	GitHash   = "undefined"
-	Started   = time.Now()
-)
-
-var Environment struct {
-	Mode string `arg:"env"`
-	Port string `arg:"env"`
-}
-
 func main() {
-	// Setup default values
-	Environment.Mode = "LOCAL" // [LOCAL, DEV, PROD]
-	Environment.Port = "8080"
-	arg.MustParse(&Environment)
+
+	flag := config.GetFlag()
+	env := config.GetEnvironment()
 
 	var logger log.Logger
 	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
@@ -41,11 +26,12 @@ func main() {
 	)
 
 	logger.Log(
-		"version", Version,
-		"gitHash", GitHash,
-		"buildTime", BuildTime,
-		"started", Started.UTC().Format(time.RFC3339),
-		"mode", Environment.Mode,
+		"version", flag.Version,
+		"gitHash", flag.GitHash,
+		"buildTime", flag.BuildTime,
+		"started", flag.Started,
+		"mode", env.Mode,
+		"port", env.Port,
 	)
 
 	// Start
@@ -60,5 +46,5 @@ func main() {
 	)
 
 	http.Handle("/api/v1/generate", generateHandler)
-	logger.Log("error", http.ListenAndServe(":"+Environment.Port, generateHandler))
+	logger.Log("error", http.ListenAndServe(env.Port, generateHandler))
 }
